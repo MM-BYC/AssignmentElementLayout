ec3cdfc173e74496bfaf886ad25bce554698b5e6
mmarque <marquezmusa@gmail.com>
2024-07-11 22:58:39 -0400

    Assignment Layout Element

---
+++ app.js
@@ -0,0 +1,14 @@
+let element = document.querySelector(".currentDate");
+let date = new Date();
+element.innerHTML =  date.toLocaleDateString();
+
+        // JavaScript to add event listener
+let hoverDiv = document.getElementById('hoverDiv');
+
+hoverDiv.addEventListener('mouseover', function() {
+hoverDiv.classList.add('quote');
+        });
+
+hoverDiv.addEventListener('mouseout', function() {
+hoverDiv.classList.remove('quote');
+        });
